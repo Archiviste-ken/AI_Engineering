567f2aa366566eb4ee991fe282b1285db2d28ca8
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-30 22:51:55 +0530

    Enhance spam email detector: Load and preprocess data, implement Bag of Words representation, and add project documentation

--- ML projects/spam_email_detector/main.py
+++ ML projects/spam_email_detector/main.py
@@ -1,6 +1,24 @@
-def main() -> None:
-    print("Spam email detector project initialized.")
+import pandas as pd
+data = pd.read_csv("data/spam.csv", encoding="latin-1")
 
+print(data.head())
 
-if __name__ == "__main__":
-    main()
+print()
+
+print(data.info())
+
+print()
+
+print(data.describe(include="all"))
+
+# Remove empty columns
+data = data.drop(columns=["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"])
+
+# Rename columns
+data.columns = ["label", "message"]
+
+print("\nAfter Cleaning:\n")
+print(data.head())
+
+print("\nColumns:")
+print(data.columns)
