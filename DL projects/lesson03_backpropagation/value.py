914f9df2017cf3ef7fea30c62dcb27e75806f505
Shreyesh <shreyeshpachpor06@gmail.com>
2026-08-03 17:27:44 +0530

    Enhance Value class with addition, multiplication, and backward methods for improved functionality

--- DL projects/lesson03_backpropagation/value.py
+++ DL projects/lesson03_backpropagation/value.py
@@ -23,9 +23,66 @@ class Value:
     def __init__(self, data, _children=(), _op=""):
 
         self.data = data
-
         self.grad = 0.0
 
         self._prev = set(_children)
-
         self._op = _op
+
+        self._backward = lambda: None
+        
+    def __add__(self, other):
+
+        out = Value(
+            self.data + other.data,
+            (self, other),
+            "+"
+        )
+
+        def _backward():
+
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+
+        return out
+    
+    def __mul__(self, other):
+
+        out = Value(
+            self.data * other.data,
+            (self, other),
+            "*"
+        )
+
+        def _backward():
+
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+    
+    def backward(self):
+
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+
+            if v not in visited:
+
+                visited.add(v)
+
+                for child in v._prev:
+                    build_topo(child)
+
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node._backward()
