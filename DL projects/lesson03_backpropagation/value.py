3db24a8a491a073d96b8cd83ada6c6aa1bf46caf
Shreyesh <shreyeshpachpor06@gmail.com>
2026-08-03 15:58:47 +0530

    Refactor Value class initialization in value.py for clarity and structure

--- DL projects/lesson03_backpropagation/value.py
+++ DL projects/lesson03_backpropagation/value.py
@@ -17,3 +17,15 @@
 # ✅ Computational Graph
 
 # This file is where most of Lesson 3 will happen.
+
+class Value:
+
+    def __init__(self, data, _children=(), _op=""):
+
+        self.data = data
+
+        self.grad = 0.0
+
+        self._prev = set(_children)
+
+        self._op = _op
